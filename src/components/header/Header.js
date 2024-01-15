5928eb57633159a55f4ed0cccad64fa5b071947e
ahmedbhatti01 <ahmed.bhatti@istoreisend.com>
2024-01-15 12:34:59 +0500

    hello

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <div>logo</div>
+        <div>button</div>
+        <div>name</div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
